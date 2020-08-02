8ed98913b2af00f46e61b6a251246ec74ca3da42
Alex Johnson <alexjohnson@MacBook-Pro.attlocal.net>
2020-08-02 10:35:23 -0400

    added a few modules to experiment with

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule
 ];
 
 @NgModule({
